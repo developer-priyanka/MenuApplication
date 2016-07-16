79893e74317f5f0e8a8bb0a5b05e6de80707c919
shafimirza <mirzashafibaig1989@gmail.com>
2016-07-17 00:44:53 +0530

    Initial commit

---
+++ app/src/main/java/my/assignment/menuapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package my.assignment.menuapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater M = getMenuInflater();
+        M.inflate(R.menu.main, menu);
+
+//        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
